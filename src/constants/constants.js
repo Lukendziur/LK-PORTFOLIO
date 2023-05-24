86897eab82d2185e9b7414b6df0dd3481b242919
Lukend <lkendziur@midasconsultores.com.ar>
2023-05-24 10:35:47 -0300

    initial settings

---
+++ src/constants/constants.js
@@ -0,0 +1,15 @@
+export const ROUTES = Object.freeze({
+    HOME: '/',
+    SKILLS: '/skills',
+    PROJECT: '/projects',
+    PROJECT_DETAIL: ':id',
+    CONTACT: '/contact'
+ });
+
+ export const NAVBAR_STRUCTURE = Object.freeze({
+    HOME: 'Inicio',
+    SKILLS: 'Habilidades',
+    PROJECT: 'Proyectos',
+    CONTACT: 'Contacto'
+ });
+
