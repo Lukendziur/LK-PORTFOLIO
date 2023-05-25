504490a72534cc60e7c3e498fc79a529af187b62
Lukend <lkendziur@midasconsultores.com.ar>
2023-05-25 11:15:43 -0300

    delete vite deps

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { minify } from 'vite-plugin-html';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    minify(), // el plugin para minificar HTML
-  ],
-  build: {
-    minify: 'terser', // Utilizar el minificador Terser para JavaScript
-    brotliSize: true, // Habilitar la compresión Brotli para archivos estáticos
-  },
+  plugins: [react()],
 });
