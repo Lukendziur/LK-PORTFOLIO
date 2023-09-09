486c5a9f1e29ad6b4bb13879f94167684a08c149
Lukend <lkendziur@midasconsultores.com.ar>
2023-09-09 20:53:39 -0300

    email form working

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,34 +1,112 @@
 // External
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { Stack, TextField, Button } from '@mui/material';
+import { DevTool } from '@hookform/devtools';
 
 // Internal
-// import { useEffect, useState } from 'react';
 import Titles from '../../components/Molecules/Titles/Titles';
-// import getCollection from '../../functions/get';
+
 // Styles
-// TODO: en contact hacer el formulario del envío de email: refs abajo
-  // ? https://medium.com/weekly-webtips/simple-react-contact-form-without-back-end-9fa06eff52d9
-  // ? https://www.abstractapi.com/guides/react-send-email-from-your-app-without-a-backend
+
 const Contact = () => {
-//   const [data, setData] = useState({});
+  const form = useRef();
 
-//   const resolveData = async () => {
-//     const projects = await getCollection();
+  const formHook = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    },
+  });
 
-//     setData(projects);
-//     return projects;
-//   };
+  const { register, handleSubmit, formState, control, reset, clearErrors } =
+    formHook;
+  const { errors } = formState;
 
-//   useEffect(() => {
-//     resolveData();
-//   }, []);
+  const sendEmail = () => {
+    emailjs
+      .sendForm(
+        'service_s4odm4r',
+        'template_obkzbof',
+        form.current,
+        '8D-bp5zmXKo-bP2Md'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          reset();
+          clearErrors();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <>
-      <Titles
-        principalTitle={'contact.title'}
-        description={'contact.description'}
-      />
-      {/* <img src={data?.nutriAr?.images[0]?.tabletImg} /> */}
+      <Titles principalTitle={'contact.title'} />
+      <form onSubmit={handleSubmit(sendEmail)} ref={form} noValidate>
+        <Stack spacing={2} width={400}>
+          <TextField
+            id="outlined-error-helper-text"
+            label="Name"
+            type="text"
+            multiline
+            {...register('name', {
+              required: 'Name is required',
+            })}
+            error={!!errors.name}
+            helperText={errors.name?.message}
+          />
+
+          <TextField
+            id="outlined-error-helper-text"
+            label="Email"
+            type="email"
+            multiline
+            {...register('email', {
+              required: 'Email is required',
+              pattern: {
+                value: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+                message: 'Entered value does not match email format',
+              },
+            })}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+          <TextField
+            id="outlined-error-helper-text"
+            label="Subject"
+            type="subject"
+            multiline
+            {...register('subject', {
+              required: 'Subject is required',
+            })}
+            error={!!errors.subject}
+            helperText={errors.subject?.message}
+          />
+          <TextField
+            id="outlined-error-helper-text"
+            label="Message"
+            type="message"
+            multiline
+            {...register('message', {
+              required: 'Message is required',
+            })}
+            error={!!errors.message}
+            helperText={errors.message?.message}
+          />
+
+          <Button type="submit" variant="contained">
+            Enviar
+          </Button>
+        </Stack>
+      </form>
+      <DevTool control={control} />
     </>
   );
 };
