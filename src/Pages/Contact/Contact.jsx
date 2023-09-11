11770ea666579adc676747a7ed6077360a9f82a9
Lukend <lkendziur@midasconsultores.com.ar>
2023-09-11 08:45:46 -0300

    animation

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,13 +1,17 @@
 // External
+import { useMediaQuery } from '@mui/material';
 
 // Internal
 import ContactForm from '../../components/Molecules/ContactForm/ContactForm';
 import Titles from '../../components/Molecules/Titles/Titles';
+import { BREAKPOINTS } from '../../constants/constants';
+import Avatar from './../../assets/callme-avatar.png';
 
 // Styles
 import styles from './Contact.module.scss';
 
 const Contact = () => {
+  const isMobile = useMediaQuery(BREAKPOINTS.mobile);
   return (
     <>
       <Titles
@@ -17,6 +21,7 @@ const Contact = () => {
       />
       <main className={styles.contactMain}>
         <ContactForm />
+        {!isMobile && <img src={Avatar} alt="Avatar" />}
       </main>
     </>
   );
