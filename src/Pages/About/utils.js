74c498c51f7169a090dabac8c2ddd7fb047efbe9
Lukend <lkendziur@midasconsultores.com.ar>
2023-09-11 20:38:57 -0300

    test3

--- src/Pages/About/utils.js
+++ src/Pages/About/utils.js
@@ -1,84 +1,107 @@
 import Css from './../../../public/assets/css-icon.svg';
+import NextLogo from './../../../public/assets/next-svg.svg';
+import Accessibility from './../../../public/assets/accessibility-web.svg';
+import AdaptativeWeb from './../../../public/assets/adaptative-web.svg';
+import ApiIcon from './../../../public/assets/api-icon.svg';
+import BootstrapIcon from './../../../public/assets/bootstrap.png';
+import CodeDoc from './../../../public/assets/code-documentation.svg';
+import FirebaseIcon from './../../../public/assets/firebase.svg';
+import GitIcon from './../../../public/assets/git-icon.svg';
+import Github from './../../../public/assets/github.svg';
+import Html from './../../../public/assets/html.svg';
+import Javascript from './../../../public/assets/javascript-icon.svg';
+import JsonIcon from './../../../public/assets/json-icon.svg';
+import MuiLogo from './../../../public/assets/MUI-logo.png';
+import NpmLogo from './../../../public/assets/npm-icon.svg';
+import Postman from './../../../public/assets/postman-icon.svg';
+import Proactive from './../../../public/assets/proactive.svg';
+import ReactIcon from './../../../public/assets/react-icon.svg';
+import ResponsiveWeb from './../../../public/assets/responsive-web.svg';
+import SassLogo from './../../../public/assets/sass-2.png';
+import Storybook from './../../../public/assets/storybook-icon.svg';
+import TeamPlayer from './../../../public/assets/team-player.svg';
+import TypescriptIcon from './../../../public/assets/typescript-icon.svg';
+import ViteLogo from './../../../public/assets/vite-logo.svg';
 
 export const skillsArray = [
   {
-    iconPath: 'next-svg.svg',
+    iconPath: NextLogo,
     alt: 'Next.js logo',
     title: 'Next.js',
   },
   {
-    iconPath: 'typescript-icon.svg',
+    iconPath: TypescriptIcon,
     alt: 'TypeScript logo',
     title: 'TypeScript',
   },
   {
-    iconPath: 'javascript-icon.svg',
+    iconPath: Javascript,
     alt: 'JavaScript logo',
     title: 'JavaScript',
   },
   {
-    iconPath: 'npm-icon.svg',
+    iconPath: NpmLogo,
     alt: 'Node Package Manager (NPM) logo',
     title: 'NPM',
   },
   {
-    iconPath: 'react-icon.svg',
+    iconPath: ReactIcon,
     alt: 'React.js logo',
     title: 'React.js',
   },
   {
-    iconPath: 'postman-icon.svg',
+    iconPath: Postman,
     alt: 'Postman logo',
     title: 'Postman',
   },
 
   {
-    iconPath: 'github.svg',
+    iconPath: Github,
     alt: 'Github logo',
     title: 'Github',
   },
   {
-    iconPath: 'api-icon.svg',
+    iconPath: ApiIcon,
     alt: 'API logo',
     title: 'APIs',
   },
   {
-    iconPath: 'git-icon.svg',
+    iconPath: GitIcon,
     alt: 'GIT logo',
     title: 'GIT',
   },
   {
-    iconPath: 'json-icon.svg',
+    iconPath: JsonIcon,
     alt: 'JSON logo',
     title: 'JSON',
   },
   {
-    iconPath: 'firebase.svg',
+    iconPath: FirebaseIcon,
     alt: 'Firebase logo',
     title: 'Firebase',
   },
   {
-    iconPath: 'vite-logo.svg',
+    iconPath: ViteLogo,
     alt: 'Vite logo',
     title: 'Vite',
   },
   {
-    iconPath: 'bootstrap.png',
+    iconPath: BootstrapIcon,
     alt: 'Bootstrap logo',
     title: 'Bootstrap',
   },
   {
-    iconPath: 'sass-2.png',
+    iconPath: SassLogo,
     alt: 'Sass logo',
     title: 'Sass',
   },
   {
-    iconPath: 'MUI-logo.png',
+    iconPath: MuiLogo,
     alt: 'Material UI logo',
     title: 'Material UI',
   },
   {
-    iconPath: 'storybook-icon.svg',
+    iconPath: Storybook,
     alt: 'Storybook logo',
     title: 'Storybook',
   },
@@ -88,38 +111,38 @@ export const skillsArray = [
     title: 'CSS',
   },
   {
-    iconPath: 'html.svg',
+    iconPath: Html,
     alt: 'HTML logo',
     title: 'HTML',
   },
   {
-    iconPath: 'code-documentation.svg',
+    iconPath: CodeDoc,
     alt: 'Code Documentation',
     title: 'Code Documentation',
   },
   {
-    iconPath: 'responsive-web.svg',
+    iconPath: ResponsiveWeb,
     alt: 'Responsive Web logo',
     title: 'Responsive Web',
   },
   {
-    iconPath: 'adaptative-web.svg',
+    iconPath: AdaptativeWeb,
     alt: 'Adaptative Design',
     title: 'Adaptative Design',
   },
   {
-    iconPath: 'accessibility-web.svg',
+    iconPath: Accessibility,
     alt: 'Accessibility Web logo',
     title: 'Accessibility Web',
   },
   {
-    iconPath: 'team-player.svg',
+    iconPath: TeamPlayer,
     alt: 'Team Player',
     title: 'Team Player',
   },
 
   {
-    iconPath: 'Proactive.svg',
+    iconPath: Proactive,
     alt: 'AProactive',
     title: 'Proactive',
   },
